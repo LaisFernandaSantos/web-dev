de8ecd547a5558047b98dcf537d305573f8f99aa
Laís <Lais07fernanda@gmail.com>
2022-10-06 12:13:26 -0300

    0.3

--- personal-portfolio/src/components/Footer.js
+++ personal-portfolio/src/components/Footer.js
@@ -16,9 +16,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+            <a href="https://www.tiktok.com/@portifolio_estudantil?lang=pt-BR"><img src={navIcon1} alt="" /></a>
+                <a href="https://twitter.com/PortifolioE"><img src={navIcon2} alt="" /></a>
+                <a href="https://www.instagram.com/portifolio_estudantil/"><img src={navIcon3} alt="" /></a>
             </div>
             <p>Criado em 2022, Todos os Direitos</p>
           </Col>
